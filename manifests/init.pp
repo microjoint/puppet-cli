750995caefbb39272acb68592fabc857224b5342
Marcus Campbell <marcus.anthony.campbell@gmail.com>
2014-08-06 18:07:29 +0200

    fix sudoers

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,6 @@ class cli (
   $path                     = $cli::params::path,
   $visual                   = $cli::params::visual,
   $umask                    = $cli::params::umask,
-  $sudo_grp_with_passwd     = $cli::params::group_with_passwd,
-  $sudo_grp_without_passwd  = $cli::params::group_without_passwd,
   $bash                     = $cli::params::bash,
   $bash_config              = $cli::params::bash_config,
 ) inherits cli::params {
@@ -13,8 +11,6 @@ class cli (
   validate_string($path)
   validate_string($visual)
   validate_string($umask)
-  validate_string($sudo_grp_with_passwd)
-  validate_string($sudo_grp_without_passwd)
   validate_bool($bash)
   validate_hash($bash_config) # i may be a hash ???
 
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,8 +3,6 @@ class cli::params {
   $path                   = '/usr/local/sbin:/usr/local/bin:/usr/bin'
   $visual                 = '/usr/bin/vim'
   $umask                  = "022"
-  $group_with_passwd      = 'wheel'
-  $group_without_passwd   = 'super_admins'
   $bash                   = true
   $bash_config            =
   {
